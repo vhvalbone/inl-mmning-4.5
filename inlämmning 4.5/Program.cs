11b0a0878305747ee13c2728ba84a03b8c44e884
alsv06003 <alsv06003@E-PW0234JW>
2023-10-20 12:38:34 +0200

    Add project files.

---
+++ inlämmning 4.5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace inlämmning_4._5
+{
+    class program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Skriv in ett heltal");
+            string heltal = Console.ReadLine();
+
+            for (int i = 0; i < heltal.Length; i++)
+            {
+                string tecken = heltal[i].ToString();
+                int siffra = int.Parse(tecken);
+                if (siffra == 9)
+                {
+                    Console.Write("0");
+                }
+                else
+                {
+                    Console.Write(siffra + 1);
+                }
+            }
+        }
+    }
+}
